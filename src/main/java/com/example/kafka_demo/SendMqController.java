ca8f3a06b68942792bf6ed404be3c1cd3793ab04
cold <86940023@qq.com>
2023-02-15 17:23:49 +0800

    feat send mq controller

--- src/main/java/com/example/kafka_demo/SendMqController.java
+++ src/main/java/com/example/kafka_demo/SendMqController.java
@@ -1,10 +1,8 @@
 package com.example.kafka_demo;
 
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -14,17 +12,19 @@ import javax.annotation.Resource;
  * </p>
  *
  * @author cold
- * @date 2023/2/14 19:07
+ * @date 2023/2/15 17:05
  */
-@Component
-public class KafkaDemoSendTask {
+@RestController
+public class SendMqController {
     @Resource
     private MsgPushMessageChannel msgPushMessageChannel;
-    @Scheduled(cron = "0/10 * * * * *")
-    void cronSchedule() {
+    @RequestMapping("/sendMq")
+    public String sendMq(){
         System.out.println("发送消息");
         KafkaDemoPayload kafkaDemoPayload = new KafkaDemoPayload();
         kafkaDemoPayload.setContent("test");
         msgPushMessageChannel.sendKafkaDemo().send(MessageBuilder.withPayload(kafkaDemoPayload).build());
+        return "ok";
+
     }
 }
